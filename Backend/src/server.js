cdf356ac040c3291136abeed79e29d14aaeffb83
phebeeee <phebe.ghattas@gmail.com>
2025-04-03 17:58:18 +0200

    fix: server.js

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,37 +1,12 @@
+
 const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
-//const connectDB = require("./config/db");
-const mongoose = require("mongoose");
-
-
+const connectDB = require("./config/db");
 
 dotenv.config();
-require("dotenv").config({ path: "./.env" });
-
-console.log("MONGO_URI from .env:", process.env.MONGO_URI); // Debugging log
-
-
-// Database connection function
-const connectDB = async () => {
-  try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      serverSelectionTimeoutMS: 30000, // Timeout after 30 seconds
-    });
-    console.log(`✅ MongoDB Connected: ${conn.connection.host}`);
-  } catch (error) {
-    console.error(`❌ Connection Error: ${error.message}`);
-    process.exit(1); // Exit if connection fails
-  }
-};
-
-// Call connectDB() before starting the server
-(async () => {
-  await connectDB();
-})();
+connectDB();
 
 const app = express();
 
@@ -45,7 +20,6 @@ app.use(cors({
 app.use(express.json());
 app.use(cors());
 app.use(cookieParser());
-
 
 app.use("/api/users", require("./routes/userRoutes"));
 app.use("/api/restaurants", require("./routes/restaurantRoutes"));
